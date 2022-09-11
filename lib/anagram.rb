cd7ef177885af8202c787f68108a26b2b67f45c0
jacksonohallo <jacksonohallo@gmail.com>
2022-09-11 20:32:02 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize (word)
+        @word = word
+    end
+    def match(array)
+        matched = []
+        array.map do |word|
+            if word.chars.sort == @word.chars.sort
+              matched.push(word)
+            
+            end
+        end
+        matched
+    end
+end
